a2d13f5ddb9e2215766d070437c0ef84a2ba9d26
mentalvenom <lukas.woodruff20@d155.org>
2018-10-01 09:35:23 -0500

    Update hello_world.js

--- examples/hello_world.js
+++ examples/hello_world.js
@@ -3,5 +3,5 @@ start.name("My Program") // Initialize Program
 {
 sys.log(sys.name.add"\n") // "My Program" // sys.name is the name of the program
 sys.log("Hello World")
-sys.halt() // halt the program
+sys.stop() // halt the program
 }
