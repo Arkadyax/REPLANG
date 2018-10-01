9bf1d38c7ac50836658a4763f10468a44fd58e5a
mentalvenom <lukas.woodruff20@d155.org>
2018-10-01 08:27:43 -0500

    Create calculator.js

---
+++ examples/calculator.js
@@ -0,0 +1,33 @@
+func numbers()
+{
+  num1: sys.input("first number: ")
+  num2: sys.input("second number: ")
+}
+func calc() // Run function "calc"
+sys.use(func.numbers())
+{
+  ask: sys.input("What operator: ") // Request user input     
+    if ask == "+" 
+    {          
+      sys.log(num1 + num2) // Like console.log()  
+    }    
+    elsif ask == "-"
+    {          
+      sys.log(num1 - num2) 
+    }  
+    elsif ask == "*"
+    {
+      sys.log(num1 * num2)
+    }
+    else
+    {
+    sys.log(num1 / num2)
+    }
+  end // End the if statement 
+} 
+sys.begin("My Program") // Initialize Program
+{
+sys.log(sys.name) // "My Program" // sys.name is the name of the program
+func.math() // run the math function  
+sys.exit() // halt the program
+}
