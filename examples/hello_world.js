9c86f168cb9ae943c9be598b02fa1e204090e6fe
mentalvenom <lukas.woodruff20@d155.org>
2018-10-01 09:32:29 -0500

    Create hello_world.js

---
+++ examples/hello_world.js
@@ -0,0 +1,7 @@
+sys.start()
+start.name("Hello World") // Initialize Program
+{
+sys.log(sys.name) // "Hello World" // sys.name is the name of the program
+sys.log("Hello World")
+sys.exit() // halt the program
+}
